6a1a71cef5d71897a576254c51dfaa919761b086
CrellanMaster <devsandrounity@gmail.com>
2024-10-10 21:37:55 -0300

    Login UI criada, criando componente de password input

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../../components/Navbar/Navbar'
+import PasswordInput from '../../components/Navbar/Input/PasswordInput'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <Navbar />
+
+      <div className='flex items-center justify-center mt-28'>
+        <div className='w-96 border rounded bg-white px-7 py-10'>
+          <form onSubmit={() => { }}>
+            <h4 className="text-2xl mb-7">Login</h4>
+
+            <input type="text" placeholder="email" className="input-box" />
+
+            <PasswordInput />
+
+            <button className="btn-primary">Login</button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet? {" "} <Link to="signup" className="font-medium text-primary underline">Create an Account</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
